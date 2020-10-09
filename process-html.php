200ab2c31c20f5d3df33c87aab157dcbc03cd536
saikiran19999 <50092036+saikiran19999@users.noreply.github.com>
2020-10-09 20:24:18 +0530

    Update process-html.php

--- process-html.php
+++ process-html.php
@@ -10,7 +10,7 @@ Hello
 ';
 // To send HTML mail, the Content-type header must be set
 $headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$headers = 'Content-type: text/html; charset=utf-8' . "\r\n";
 // Additional headers as http://php.net/manual/en/function.mail.php
 $headers .= 'From: Your Name <saikiran.modireddy@gmail>' . "\r\n";
 //ACTIVE mail below to $admin (you) and $email (the other person)
